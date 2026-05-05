a7495bc71aba51b726abcc54a8b55f6668886c29
Cherrisita <cherry0jeva@gmail.com>
2026-05-05 00:01:09 -0600

    canny + labeled

--- preprocess.py
+++ preprocess.py
@@ -15,13 +15,32 @@ def process_images(input_path, output_path):
 
         binary = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                    cv2.THRESH_BINARY, 11, 2)
+        
+        blur = cv2.GaussianBlur(gray, (5, 5), 0)
+
+        edges = cv2.Canny(blur, 100, 200)
+
+        num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(binary)
+
+        output = img.copy()
+
+        for i in range(1, num_labels):
+            area = stats[i, cv2.CC_STAT_AREA]
+
+            if area >= 500:
+                x = stats[i, cv2.CC_STAT_LEFT]
+                y = stats[i, cv2.CC_STAT_TOP]
+                w = stats[i, cv2.CC_STAT_WIDTH]
+                h = stats[i, cv2.CC_STAT_HEIGHT]
+
+                cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 1)
 
         os.makedirs(output_path, exist_ok = True)
 
-        cv2.imwrite(os.path.join(output_path, archive), binary)
+        cv2.imwrite(os.path.join(output_path, archive), edges)
+        cv2.imwrite(os.path.join(output_path, 'labeled_' + archive), output)
 
 
 for i in range(4):
     process_images('dataset/cat/' + cats[i], 'processed/cat/' + cats[i])
     process_images('dataset/dog/' + dogs[i], 'processed/dog/' + dogs[i])    
-        
--- preprocess_example.py
+++ preprocess_example.py
@@ -1,5 +1,6 @@
-import os
+import numpy as np
 import cv2
+
 
 img = cv2.imread('example/original.jpg')
 
@@ -18,3 +19,32 @@ cv2.imwrite('example/binary.jpg', binary)
 binary_adaptive = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
 cv2.imwrite('example/binary_adaptive.jpg', binary_adaptive)
+
+blur = cv2.GaussianBlur(gray, (5, 5), 0)
+
+edges = cv2.Canny(blur, 100, 200)
+
+cv2.imwrite('example/edges.jpg', edges)
+
+num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(binary_adaptive)
+
+output = reescaled.copy()
+
+for i in range(1, num_labels):
+    area = stats[i, cv2.CC_STAT_AREA]
+    print(area)
+
+    if area >= 500:
+        x = stats[i, cv2.CC_STAT_LEFT]
+        y = stats[i, cv2.CC_STAT_TOP]
+        w = stats[i, cv2.CC_STAT_WIDTH]
+        h = stats[i, cv2.CC_STAT_HEIGHT]
+
+        cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 1)
+
+if labels.max() > 0:
+    label_img = np.uint8(255 * labels / labels.max())
+else:
+    label_img = labels
+
+cv2.imwrite('example/labels.jpg', output)
